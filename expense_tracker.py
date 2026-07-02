9aa71e9c488365a72fba1af6a7202585e3d54b1e
John Unuigbe <junuig2@wgu.edu>
2026-07-02 11:34:08 -0400

    Add save and load CSV persistence

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,9 +1,10 @@
+import csv
 expenses = []
 def add_expense(date, time, location, store, amount):
     if len(expenses) == 0:
         id = 1
     else:
-        id = expenses[-1]["id"] + 1
+        id = int(expenses[-1]["id"]) + 1
     expense = {
         "id": id,
         "date": date,
@@ -18,6 +19,23 @@ def list_expenses():
     for expense in expenses:
         print(f"ID: {expense['id']} | Date: {expense['date']} | Time: {expense['time']} | Location: {expense['location']} | Store: {expense['store']} | Amount: ${expense['amount']}")
 
+def save_expenses():
+    with open("expenses.csv", "w") as file:
+        writer = csv.DictWriter(file, fieldnames=["id", "date", "time", "location", "store", "amount"])
+        writer.writeheader()
+        writer.writerows(expenses)
+
+def load_expenses():
+    try:
+        with open("expenses.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                expenses.append(row)
+    except FileNotFoundError:
+            pass
+
+load_expenses()
 add_expense("01-15-2026", "12:00", "Columbus, Ohio", "Marshalls", 85.00)
 add_expense("07-01-2026", "03:14", "Columbus, Ohio", "Saks Fifth Avenue", 1295.00)
 list_expenses()
+save_expenses()
